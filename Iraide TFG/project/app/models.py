6485f46a72197996d6d95e7a5fb694b109381b37
iraidehp <i.hoyas.2019@alumnos.urjc.es>
2023-12-30 15:10:29 +0100

    version 2

--- Iraide TFG/project/app/models.py
+++ Iraide TFG/project/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class usuario(models.Model):
+    nombre = models.CharField(max_length=20)
+    edad = models.IntegerField()
+    pais = models.CharField(max_length=20)
+    sexo = models.CharField(max_length=20)
+    so = models.CharField(max_length=20)
--- Iraide TFG/project/project/settings.py
+++ Iraide TFG/project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fx8800w*@n5jhhj99=jf*5_b4-3k#$n3kbk@p3m!oxh56fz#g#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [''],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ES'
 
 TIME_ZONE = 'UTC'
 
